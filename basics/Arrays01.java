8121b13c9b8af82e2c9713f6c04de6e5f0850e16
Sanya Chauhan <sanyachauhan453@gmail.com>
2025-07-11 15:32:00 +0000

    JAVA

---
+++ basics/Arrays01.java
@@ -0,0 +1,17 @@
+public class Arrays01{
+
+    static void change_array(int[]arr){
+        for(int i=0;i<arr.length;i++){
+            arr[i]=0;
+        }
+    }
+    
+    public static void main(String[]args){
+        int []arr=new int[3];
+        arr[0]=5;
+        arr[1]=6;
+        arr[2]=7;
+
+        change_array(arr);
+    }
+}
